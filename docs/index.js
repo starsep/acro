99987e3db7d05cb22452534831a67d05cb218ba9
Filip Czaplicki <filipczaplicki@gmail.com>
2018-06-09 20:19:26 +0200

    Init

---
+++ docs/index.js
@@ -0,0 +1,54 @@
+var map = new L.Map('map', {center: new L.LatLng(52.237049, 21.017532), zoom: 12});
+var roads = L.gridLayer.googleMutant({
+  type: 'roadmap' // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
+}).addTo(map);
+map.addLayer(roads);
+
+function getJSON(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+  xhr.onload = function() {
+    var status = xhr.status;
+    if (status === 200) {
+      callback(null, xhr.response);
+    } else {
+      callback(status, xhr.response);
+    }
+  };
+  xhr.send();
+}
+
+function showPoints(points) {
+  let redIcon = L.icon({
+    iconUrl: 'leaf-red.png',
+    shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
+
+  for (let i = 0; i < points.length; i++) {
+    let point = points[i];
+    L.marker([point.lat, point.lng], {icon: redIcon}).addTo(map);
+  }
+}
+
+getJSON('bikes.json',
+  function(err, data) {
+    if (err !== null) {
+      alert("Error: " + err);
+    } else {
+      let bikes = data.bikes;
+      for (let i = 0; i < bikes.length; i++) {
+        let bike = bikes[i];
+        let marker = L.marker([bike.lat, bike.lng]).addTo(map);
+      }
+      // showPoints(data.points);
+    }
+  }
+);
+
